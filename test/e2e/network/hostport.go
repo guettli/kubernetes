8c37fc640a0e9827c40239ebf88bd885137b9e2a
Thomas Güttler <thomas.guettler@syself.com>
2026-02-04 16:36:07 +0100

    make diff smaller.

--- test/e2e/network/hostport.go
+++ test/e2e/network/hostport.go
@@ -216,6 +216,6 @@ func createHostPortPodOnNode(ctx context.Context, f *framework.Framework, podNam
 	}
 
 	if err := e2epod.WaitTimeoutForPodReadyInNamespace(ctx, f.ClientSet, podName, ns, framework.PodStartTimeout); err != nil {
-			framework.Failf("createHostPortPodOnNode: pod %s (%s:%d) failed or timeout, err:%v", podName, hostIP, port, err)
+		framework.Failf("createHostPortPodOnNode: pod %s (%s:%d) failed or timeout, err:%v", podName, hostIP, port, err)
 	}
 }
